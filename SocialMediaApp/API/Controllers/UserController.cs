337897c3627c0cf853de01b62d369e0aa60d46b0
jfarro01 <jfarro01.jose.nicolas.farro.a@gmail.com>
2019-10-31 15:18:48 -0500

    Added .runsettings file and global.json, extended implementation of UserController in API

--- SocialMediaApp/API/Controllers/UserController.cs
+++ SocialMediaApp/API/Controllers/UserController.cs
@@ -12,6 +12,13 @@ namespace API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+
+    /*
+ 
+       * Get FriendList
+       
+    */
+
     public class UserController : ControllerBase
     {
         private readonly IUserRepository _userRepo;
@@ -21,6 +28,7 @@ namespace API.Controllers
             _userRepo = urepo ?? throw new ArgumentNullException(nameof(urepo));
         }
 
+        // Get UserPosts
         // GET: api/User
         [HttpGet]
         public IEnumerable<string> Get()
@@ -28,18 +36,42 @@ namespace API.Controllers
             return new string[] { "value1", "value2" };
         }
 
+        // Get User by Name
         // GET: api/User/5
         [HttpGet("{id}", Name = "Get")]
         public UserViewModel Get(int id)
         {
+            var x = _userRepo.GetUserByID(id);
+            
+            return new UserViewModel()
+            {
+                Username = x.Username,
+                FirstName = x.FirstName,
+                LastName = x.LastName,
+                Gender = x.Gender,
+                Email = x.Email,
+                Id = x.UserID        
+            };
 
-            return new UserViewModel();
         }
 
+        // Post User Model
         // POST: api/User
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult Post([FromBody, Bind("FirstName, LastName, Username, Email, Gender")] UserViewModel newUser)
         {
+            Library.Models.User mappedUser = new Library.Models.User()
+            {
+                Username = newUser.Username,
+                FirstName = newUser.FirstName,
+                LastName = newUser.LastName,
+                Gender = newUser.Gender,
+                Email = newUser.Email,
+                UserID = newUser.Id
+
+            };
+            _userRepo.AddUser(mappedUser);
+            return CreatedAtRoute("Get", new { Id = mappedUser.UserID}, newUser);
         }
 
         // PUT: api/User/5
