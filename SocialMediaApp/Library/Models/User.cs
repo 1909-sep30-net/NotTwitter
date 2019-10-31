a9c8e606d5d50bb0faedca5b2c150b405884c8cc
yetinight <gang@ucsd.edu>
2019-10-31 14:08:54 -0500

    changed email regex to use MailAddress method instead

--- SocialMediaApp/Library/Models/User.cs
+++ SocialMediaApp/Library/Models/User.cs
@@ -77,15 +77,15 @@ namespace Library.Models
             }
             set
             {
-                if (Regex.IsMatch(value, @"^([a-zA-Z0-9_-.]+)@(([[0-9]{1,3}" +
-                            @".[0-9]{1,3}.[0-9]{1,3}.)|(([a-zA-Z0-9-]+" +
-                            @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(]?)$" +
-                            @"^([a-zA-Z0-9_-.]+)@(([[0-9]{1,3}" +
-                            @".[0-9]{1,3}.[0-9]{1,3}.)|(([a-zA-Z0-9-]+" +
-                            @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(]?)$"))
+                try
                 {
-                    _email = value;
+                    System.Net.Mail.MailAddress m = new System.Net.Mail.MailAddress(value);
                 }
+                catch (FormatException ex)
+                {
+                    throw ex;
+                }
+                _email = value;
             }
         }
 
